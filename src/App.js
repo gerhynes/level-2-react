024c48d5d0416fb671d1651e321b322c144d494b
Gerard Hynes <gkhynes@gmail.com>
2020-02-01 18:59:26 +0000

    Add reusable toggle with children render props

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import React from "react";
-import ToggleRenderProps from "./ToggleRenderProps";
+import Toggle from "./ToggleRPC";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ToggleRenderProps
-          render={({ on, toggle }) => (
+        <Toggle>
+          {({ on, toggle }) => (
             <div>
               {on && <h1>Now you see me</h1>}
               <button onClick={toggle}>Show/Hide</button>
             </div>
           )}
-        />
+        </Toggle>
       </header>
     </div>
   );
