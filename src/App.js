1d5816de782423f923924278c9713d9d94a61250
Gerard Hynes <gkhynes@gmail.com>
2020-02-01 18:46:15 +0000

    Add Render Prop Toggle

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ToggleRenderProps from "./ToggleRenderProps";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <ToggleRenderProps
+          render={({ on, toggle }) => (
+            <div>
+              {on && <h1>Now you see me</h1>}
+              <button onClick={toggle}>Show/Hide</button>
+            </div>
+          )}
+        />
       </header>
     </div>
   );
