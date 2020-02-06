bf38a6774599f429732cd6458b4b559d1cd521c7
Gerard Hynes <gkhynes@gmail.com>
2020-02-06 19:28:50 +0000

    USe Context to update user data

--- src/App.js
+++ src/App.js
@@ -12,11 +12,19 @@ class UserProvider extends Component {
     email: "scott@leveluptuts.com"
   };
 
+  logout = () =>
+    this.setState({
+      id: null,
+      name: "",
+      email: ""
+    });
+
   render() {
     return (
       <UserContext.Provider
         value={{
-          user: this.state
+          user: this.state,
+          logout: this.logout
         }}
       >
         {this.props.children}
