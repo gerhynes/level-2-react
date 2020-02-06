d9f04ee1e7d7b332d7e8e2336c62996c4cd129ec
Gerard Hynes <gkhynes@gmail.com>
2020-02-06 19:18:34 +0000

    Fetch user data with Context API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { createContext, Component } from "react";
 import { Toggle } from "utilities";
 import { Modal } from "elements";
+import User from "./User";
+import { UserContext } from "./UserContext";
 import "./App.css";
+
+class UserProvider extends Component {
+  state = {
+    id: "123",
+    name: "Scott",
+    email: "scott@leveluptuts.com"
+  };
+
+  render() {
+    return (
+      <UserContext.Provider
+        value={{
+          user: this.state
+        }}
+      >
+        {this.props.children}
+      </UserContext.Provider>
+    );
+  }
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Toggle>
-          {({ on, toggle }) => (
-            <>
-              <button onClick={toggle}>Login</button>
-              <Modal on={on} toggle={toggle}>
-                <h1>Hi from inside the Modal</h1>
-              </Modal>
-            </>
-          )}
-        </Toggle>
-      </header>
-    </div>
+    <UserProvider>
+      <div className="App">
+        <header className="App-header">
+          <User />
+          <Toggle>
+            {({ on, toggle }) => (
+              <>
+                <button onClick={toggle}>Login</button>
+                <Modal on={on} toggle={toggle}>
+                  <h1>Hi from inside the Modal</h1>
+                </Modal>
+              </>
+            )}
+          </Toggle>
+        </header>
+      </div>
+    </UserProvider>
   );
 }
 
